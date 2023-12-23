b947c0ec331247e5d6791dbbfd1a71f21187901e
jinki <jkhsky121@gmail.com>
2023-12-23 15:49:02 +0900

    Unit testing the UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -70,10 +70,8 @@ export class UsersService {
 
   async findById(id: number): Promise<UserProfileOutput> {
     try {
-      const user = await this.users.findOne({ where: { id } });
-      if (user) {
-        return { ok: true, user: user };
-      }
+      const user = await this.users.findOneOrFail({ where: { id } });
+      return { ok: true, user };
     } catch (error) {
       return { ok: false, error: 'User Not Found' };
     }
@@ -117,7 +115,7 @@ export class UsersService {
       }
       return { ok: false, error: 'Verification not found.' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'Could not verify email.' };
     }
   }
 }
