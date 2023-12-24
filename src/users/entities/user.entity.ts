5c06d70174d35254e8ab341c5a75a55f5949b98c
jinki <jkhsky121@gmail.com>
2023-12-24 13:21:57 +0900

    e2e testing UsersModule

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,11 +4,11 @@ import {
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
-import { CoreEntity } from 'src/common/entities/core.entity';
 import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
 import { IsEmail, IsEnum } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
 
 enum UserRole {
   Client,
@@ -23,7 +23,7 @@ registerEnumType(UserRole, { name: 'UserRole' });
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
-  @Column()
+  @Column({ unique: true })
   @Field(() => String)
   @IsEmail()
   email: string;
